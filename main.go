06f91d6d8cb285cff52d500bf79d9a490127fe4b
Meiro <luminovoez@gmail.com>
2023-07-17 23:37:49 +0800

    gin

--- main.go
+++ main.go
@@ -1,91 +1,15 @@
 package main
 
 import (
-	"log"
-	"net/url"
-
-	"github.com/gofiber/fiber/v2"
-
-	"github.com/Hana-ame/fedi-antenna/user"
-	"github.com/Hana-ame/fedi-antenna/webfinger"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-
-	log.Println("Starting")
-
-	app := fiber.New()
-
-	// app.All(webfinger.WebFingerPath, func(c *fiber.Ctx) error {
-	// 	return nil
-	// })
-	app.Mount(webfinger.WebFingerPath, webfingerApp())
-	app.Use("/", func(c *fiber.Ctx) (err error) {
-		log.Println(c.Path())
-		// c.SendString(c.Path())
-		return c.Next()
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
 	})
-
-	app.All("/users/:username", func(c *fiber.Ctx) (err error) {
-		username := c.Params("username")
-		if username == "" {
-			return nil
-		}
-
-		as, err := user.UserAS(username)
-		if err != nil {
-			return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
-				"error": err.Error(),
-			})
-		}
-
-		c.Set("Content-Type", `application/ld+json; profile="https://www.w3.org/ns/activitystreams"; charset=utf-8`)
-		_, err = c.Status(fiber.StatusOK).Write(as)
-		if err != nil {
-			return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
-				"error": err.Error(),
-			})
-		}
-
-		return nil
-	})
-
-	log.Fatal(app.Listen(":5000"))
-}
-
-func webfingerApp() *fiber.App {
-	app := fiber.New()
-
-	app.All("", func(c *fiber.Ctx) error {
-		acct := c.Query("resource")
-
-		acct, err := url.PathUnescape(acct)
-		if err != nil {
-			return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
-				"error": "invalid_resource",
-			})
-		}
-
-		// log.Println(acct)
-
-		username, domain := webfinger.ParseAcct(acct)
-
-		if !webfinger.CheckDomain(domain) {
-			return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
-				"error": "invalid_resource",
-			})
-		}
-
-		if !webfinger.CheckUserExist(username) {
-			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-				"error": "no_such_user",
-			})
-		}
-
-		err = c.Status(fiber.StatusOK).JSON(webfinger.GetResource(username, domain))
-
-		return err
-	})
-
-	return app
+	r.Run("0.0.0.0:3000") // listen and serve on 0.0.0.0:3000
 }
