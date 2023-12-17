cb25124bfb65d2b92f97794fff027212651af305
lumino <luminovoez@gmail.com>
2023-12-17 19:25:28 +0800

    `npm run dev` is avaliable.

---
+++ log.md
@@ -0,0 +1,5 @@
+要先 `npm install`
+
+跟着提示跑了 `npm audit fix`
+
+`npm run dev`
--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { useState } from 'react';
 import styles from './App.module.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(9);
 
   return (
     <div className={styles['App']}>
       <h1 className={styles['app-heading']}>Vite + React + TS + Tailwind</h1>
       <div>
-        <button className={styles['button']} onClick={() => setCount((count) => count + 1)}>
+        <button className={styles['button']} onClick={() => setCount((count) => count - 1)}>
           count is {count}
         </button>
         <p>
