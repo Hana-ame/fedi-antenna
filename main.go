b355214f10487e6a91e1f406f8e7f1ab62b75fc2
lumino <luminovoez@gmail.com>
2023-12-28 08:47:49 +0800

    rearrange variables location

--- main.go
+++ main.go
@@ -1,25 +1,16 @@
 package webfinger
 
 import (
-	"net/http"
-
 	"github.com/Hana-ame/fedi-antenna/webfinger/actions"
 	"github.com/Hana-ame/fedi-antenna/webfinger/controller"
 	"github.com/gin-gonic/gin"
 )
 
-func Register(r *gin.Engine) {
+func RegistPath(r *gin.Engine) {
 	r.GET("/webfinger", controller.Webfinger)
 }
 
-func main() {
-	r := gin.Default()
-
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "hello world")
-	})
-
-	actions.FetchWebfingerByAcct("")
-
-	r.Run()
-}
+var (
+	// methods
+	FetchWebfingerByAcct = actions.FetchWebfingerByAcct
+)
