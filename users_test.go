5a19f6160f6e949c919cc0b7353b01abc944eacf
lumino <luminovoez@gmail.com>
2024-01-02 17:22:39 +0800

    change interface(not tested)

--- users_test.go
+++ users_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestUser(t *testing.T) {
 	id := "https://fedi.moonchan.xyz/users/test1"
-	user, err := core.ReadActivitypubUserByID(id, true)
+	user, err := core.ReadActivitypubUserByID(id)
 	fmt.Println(user)
 	fmt.Println(err)
 }
