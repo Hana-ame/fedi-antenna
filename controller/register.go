fa7a93c8cb1c43392fc57e08d4552855a8cd067b
lumino <luminovoez@gmail.com>
2024-01-05 03:05:12 +0800

    c.Bind with form

--- controller/register.go
+++ controller/register.go
@@ -3,42 +3,17 @@ package controller
 import (
 	"net/http"
 
+	"github.com/Hana-ame/fedi-antenna/antenna/model"
 	handler "github.com/Hana-ame/fedi-antenna/core/handler/antenna"
-	"github.com/Hana-ame/fedi-antenna/core/utils"
 	"github.com/gin-gonic/gin"
 )
 
 func Register(c *gin.Context) {
-	var username string
-	var host string
-	var email string
-	var passwd string
 
-	p := &utils.Parser{Context: c}
+	var o *model.Register
+	c.Bind(&o)
 
-	if err := p.PostForm("username", &username); err != nil {
-		c.JSON(http.StatusBadRequest, err.Error())
-		return
-	}
-	if err := p.PostForm("email", &email); err != nil {
-		c.JSON(http.StatusBadRequest, err.Error())
-		return
-	}
-	if err := p.PostForm("host", &host); err != nil {
-		c.JSON(http.StatusBadRequest, err.Error())
-		return
-	}
-	if err := p.PostForm("passwd", &passwd); err != nil {
-		c.JSON(http.StatusBadRequest, err.Error())
-		return
-	}
-
-	if err := handler.Register(
-		username,
-		host,
-		email,
-		passwd,
-	); err != nil {
+	if err := handler.Register(o); err != nil {
 		c.JSON(http.StatusInternalServerError, err.Error())
 		return
 	}
