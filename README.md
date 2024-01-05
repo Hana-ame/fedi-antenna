abea89cf98e05d16b47473bacd4e5a55043d7b88
lumino <luminovoez@gmail.com>
2024-01-05 20:39:35 +0800

    acitivitypub/controller还没测试

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 the api by myself.
 used in webapp
+some are reuse back/mastodon
+
+## implements
+
+- regist
--- controller/note_test.go
+++
@@ -1,3 +0,0 @@
-package controller
-
-var postNoteText = `{"status":"1","in_reply_to_id":null,"media_ids":[],"sensitive":false,"spoiler_text":"","visibility":"direct","poll":null,"language":"zh"}`
