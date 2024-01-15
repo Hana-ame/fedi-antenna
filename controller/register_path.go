e4c6ff9e815cc84e8d536abcdec0d2d9ed250f05
lumino <luminovoez@gmail.com>
2024-01-15 10:17:17 +0800

    add path

--- controller/register_path.go
+++ controller/register_path.go
@@ -1,11 +1,22 @@
 package mastodon
 
 import (
-	"github.com/Hana-ame/fedi-antenna/mastodon/controller/statuses"
+	accounts "github.com/Hana-ame/fedi-antenna/mastodon/controller/accounts"
+	statuses "github.com/Hana-ame/fedi-antenna/mastodon/controller/statuses"
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterPath(r *gin.Engine) {
+	// account
+	r.POST("/api/v1/accounts/:id/follow", accounts.Follow_account)
+	r.POST("/api/v1/accounts/:id/unfollow", accounts.Unfollow_account)
 	// statuses
-	r.POST("/api/v1/statuses", statuses.Post_a_new_status) // inbox
+	r.POST("/api/v1/statuses", statuses.Post_a_new_status)
+	r.DELETE("/api/v1/statuses/:id", statuses.Delete_a_status)
+	// reblog
+	r.POST("/api/v1/statuses/:id/reblog", statuses.Boost_a_status)
+	r.POST("/api/v1/statuses/:id/unreblog", statuses.Undo_boost_of_a_status)
+	// favourite
+	r.POST("/api/v1/statuses/:id/favourite", statuses.Favourite_a_status)
+	r.POST("/api/v1/statuses/:id/unfavourite", statuses.Undo_favourite_of_a_status)
 }
