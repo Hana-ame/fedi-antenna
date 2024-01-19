0b6b271ec7dabdc6c4bb1d28ce72b2d5cca93f88
lumino <luminovoez@gmail.com>
2024-01-19 13:46:20 +0800

    修改了db，调整到能够编译了

--- handler/register.go
+++ handler/register.go
@@ -5,33 +5,46 @@ import (
 
 	tools "github.com/Hana-ame/fedi-antenna/Tools"
 	"github.com/Hana-ame/fedi-antenna/antenna/model"
-	"github.com/Hana-ame/fedi-antenna/core/convert"
 	"github.com/Hana-ame/fedi-antenna/core/dao"
 	core "github.com/Hana-ame/fedi-antenna/core/model"
 	"github.com/Hana-ame/fedi-antenna/core/utils"
+	"github.com/Hana-ame/fedi-antenna/mastodon/entities"
 )
 
 func Register(o *model.Register) error {
 	id := utils.ParseActivitypubID(o.Username, o.Host)
 	pk := utils.GeneratePrivateKey()
+	now := utils.Now()
 	user := &core.LocalUser{
-		Email:             o.Email,
-		PasswdHash:        tools.Hash(o.Passwd, os.Getenv("SALT")),
-		ID:                id,
-		PreferredUsername: o.Username,
-		Published:         utils.Now(),
-		PrivateKeyPem:     utils.MarshalPrivateKey(pk),
-		PublicKeyPem:      utils.MarshalPublicKey(&pk.PublicKey),
+		Email:         o.Email,
+		PasswdHash:    tools.Hash(o.Passwd, os.Getenv("SALT")),
+		ActivitypubID: id,
+		AccountID:     utils.TimestampToRFC3339(now),
+		Username:      o.Username,
+		Host:          o.Host,
+		CreatedAt:     now,
+		PrivateKeyPem: utils.MarshalPrivateKey(pk),
+		// PublicKeyPem:  utils.MarshalPublicKey(&pk.PublicKey),
 	}
 
+	acct := &entities.Account{
+		Id:       utils.TimestampToRFC3339(user.CreatedAt),
+		Username: user.Username,
+		Acct:     utils.ParseAcctStr(user.Username, user.Host),
+		Url:      utils.ParseProfileUrl(user.Username, user.Host),
+		Uri:      user.AccountID,
+	}
 	if err := dao.Create(user); err != nil {
 		return err
 	}
-
-	activitypubUser := convert.ToActivityPubUser(user)
-	if err := dao.Create(activitypubUser); err != nil {
+	if err := dao.Create(acct); err != nil {
 		return err
 	}
+
+	// activitypubUser := convert.ToActivityPubUser(user)
+	// if err := dao.Create(activitypubUser); err != nil {
+	// 	return err
+	// }
 
 	return nil
 }
